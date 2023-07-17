ecd4a12c68a666ddd67fa826fe9c916ef66322c3
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-07-17 15:32:43 +0300

    Songs fetching & list displaying

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,13 +7,16 @@ import {BiSearch} from "react-icons/bi";
 import Box from "@/components/Box";
 import SidebarItem from "@/components/SidebarItem";
 import Library from "@/components/Library";
+import {Song} from "@/types";
 
 interface SidebarProps {
     children: React.ReactNode;
+    songs: Song[]
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
-    children
+    children,
+    songs
                                          }) => {
     const pathname = usePathname();
 
@@ -67,7 +70,7 @@ const Sidebar: React.FC<SidebarProps> = ({
                     </div>
                 </Box>
                 <Box className="overflow-y-auto h-full">
-                    <Library/>
+                    <Library songs={songs}/>
                 </Box>
             </div>
             <main
