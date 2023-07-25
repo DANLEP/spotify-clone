6026ec805e06d8f783fd444273a181441a7a1d8c
Daniil M <41665964+DANLEP@users.noreply.github.com>
2023-07-25 12:57:07 +0300

    Account page

---
+++ app/account/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Header from "@/components/Header";
+import AccountContent from "@/app/account/components/AccountContent";
+
+const Account = () => {
+    return (
+        <div
+            className="
+                bg-neutral-900
+                rounded-lg
+                h-full
+                w-full
+                overflow-hidden
+                overflow-y-auto
+            ">
+            <Header classname="from-bg-neutral-900">
+                <div className="mb-2 flex flex-col gap-y-6">
+                    <h1 className="text-white text-3xl font-semibold">
+                        Account settings
+                    </h1>
+                </div>
+            </Header>
+            <AccountContent/>
+        </div>
+    );
+};
+
+export default Account;
